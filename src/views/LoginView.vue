728e9bd2d76c74b1b4847ac4adf4d1d7c253f0f2
JuleZ_2k4 <8096@htl.rennweg.at>
2022-04-05 22:52:05 +0200

    schnittstelle implementiert

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,18 +5,39 @@
       ...or
       <router-link to="/register" class="underline">Register</router-link>
     </h2>
-
-    <form>
+    <form @submit.prevent="login()">
       <div class="mb-6">
         <label
           for="email"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          class="
+            block
+            mb-2
+            text-sm
+            font-medium
+            text-gray-900
+            dark:text-gray-300
+          "
           >Your email</label
         >
         <input
           type="email"
           id="email"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="
+            bg-gray-50
+            border border-gray-300
+            text-gray-900 text-sm
+            rounded-lg
+            focus:ring-blue-500 focus:border-blue-500
+            block
+            w-full
+            p-2.5
+            dark:bg-gray-700
+            dark:border-gray-600
+            dark:placeholder-gray-400
+            dark:text-white
+            dark:focus:ring-blue-500
+            dark:focus:border-blue-500
+          "
           placeholder="name@flowbite.com"
           required
           v-model="email"
@@ -25,13 +46,35 @@
       <div class="mb-6">
         <label
           for="password"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          class="
+            block
+            mb-2
+            text-sm
+            font-medium
+            text-gray-900
+            dark:text-gray-300
+          "
           >Your password</label
         >
         <input
           type="password"
           id="password"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="
+            bg-gray-50
+            border border-gray-300
+            text-gray-900 text-sm
+            rounded-lg
+            focus:ring-blue-500 focus:border-blue-500
+            block
+            w-full
+            p-2.5
+            dark:bg-gray-700
+            dark:border-gray-600
+            dark:placeholder-gray-400
+            dark:text-white
+            dark:focus:ring-blue-500
+            dark:focus:border-blue-500
+          "
           required
           v-model="password"
         />
@@ -42,8 +85,20 @@
             id="remember"
             aria-describedby="remember"
             type="checkbox"
-            class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+            class="
+              w-4
+              h-4
+              bg-gray-50
+              rounded
+              border border-gray-300
+              focus:ring-3 focus:ring-blue-300
+              dark:bg-gray-700
+              dark:border-gray-600
+              dark:focus:ring-blue-600
+              dark:ring-offset-gray-800
+            "
             required
+            checked="checked"
           />
         </div>
         <div class="ml-3 text-sm">
@@ -55,9 +110,22 @@
         </div>
       </div>
       <button
-        type="submit"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        @click="login()"
+      type="submit"
+      class="
+          text-white
+          bg-blue-700
+          hover:bg-blue-800
+          focus:ring-4 focus:outline-none focus:ring-blue-300
+          font-medium
+          rounded-lg
+          text-sm
+          w-full
+          sm:w-auto
+          px-5
+          py-2.5
+          text-center
+          dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+        "
       >
         Submit
       </button>
@@ -66,19 +134,32 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: "LoginView",
   data() {
     return {
-      email: "",
-      password: "",
+      email: "yes@gmail.com",
+      password: "YOMAMA",
     };
   },
   methods: {
-    login() {
+    async login() {
       console.log("Logging in...");
       console.log(this.email);
       console.log(this.password);
+      // api call to login php page
+        axios.post('http://localhost:8080/', {
+          action: 'login',
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          console.log(response.config);
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
   },
 };
