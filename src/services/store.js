fd2d8fb200e17fed5cafaeedebc9592ae6d64e96
Nekonyx13 <corvin.prath@gmail.com>
2022-04-26 11:20:10 +0200

    register fix

---
+++ src/services/store.js
@@ -0,0 +1,36 @@
+/**
+ * Vuex store example
+ */
+export default {
+  /** Application state */
+  state: {
+    hostData: {},
+  },
+
+  /** Methods that read the application state */
+  getters: {
+    friendlyCounter(state) {
+      return `counter = ${state.counter}`;
+    },
+  },
+
+  /** Methods that change the application state synchronously */
+  mutations: {
+    setCounter(state, counter) {
+      state.counter = counter;
+    },
+
+    count(state) {
+      state.counter++;
+    },
+  },
+
+  /** Methods that change the application state asynchronously */
+  actions: {
+    countLater({ commit }) {
+      setTimeout(() => {
+        commit("count");
+      }, 1000);
+    },
+  },
+};
