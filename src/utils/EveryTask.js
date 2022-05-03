4e6e2db2869fd108bdef366a84382a5fc12183db
Nekonyx13 <corvin.prath@gmail.com>
2022-05-03 10:45:40 +0200

    addTask form added

--- src/utils/EveryTask.js
+++ src/utils/EveryTask.js
@@ -1,39 +1,36 @@
-import axios from "axios"
+import axios from "axios";
 
 export default class EveryTask {
+  login(email, password) {
+    axios
+      .post("http://localhost:8080", {
+        action: "login",
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    login(email, password) {
-        axios
-            .post("http://localhost:8080", {
-                action: 'login',
-                email: email,
-                password: password,
-            })
-            .then((response) => {
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
+  register(email, password, username) {
+    axios
+      .post("http://localhost:8080/", {
+        action: "register",
+        email: email,
+        password: password,
+        username: username,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    register(email, password, username) {
-        axios
-        .post("http://localhost:8080/", {
-          action: "register",
-          email: email,
-          password: password,
-          username: username,
-        })
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-
-    getTasks() {
-
-    }
+  getTasks() {}
 }
--- src/views/modals/AddTask.vue
+++ src/views/modals/AddTask.vue
@@ -1,14 +1,67 @@
 <template>
-  <router-link to="/.." class="btn btn-primary">
+  <router-link to="/tasks" class="btn btn-primary">
     <i class="fas fa-arrow-left"></i>
     Back
   </router-link>
-  <div class="container">HALLO DU WILLST WAS HINZUFÜGEN</div>
+  <div class="container bg-slate-600">
+    <h1>Adding a Task</h1>
+    <div class="flex flex-col">
+      <label for="title">Title</label>
+      <input type="text" v-model="task.title" placeholder="Task Title" />
+
+      <label for="description">Description</label>
+      <textarea
+        v-model="task.description"
+        placeholder="Task Description"
+      ></textarea>
+
+      <label for="dueDate">Due Date</label>
+      <input ref="dueDate" type="date" v-model="task.dueDate" />
+
+      <label for="createDate">Start Date</label>
+      <input ref="createdDate" type="date" v-model="task.createDate" />
+
+      <label for="notes">Notes</label>
+      <textarea v-model="task.notes" placeholder="Notes"></textarea>
+
+      <button class="bg-cornflower p-4 mx-auto" @click="addTask">Add Task</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AddTask",
+
+  data() {
+    return {
+      task: {
+        title: "",
+        description: "",
+        dueDate: "",
+        createDate: "",
+        note: "",
+      },
+    };
+  },
+
+  mounted() {
+    this.$refs.createdDate.valueAsDate = new Date();
+    this.$refs.dueDate.valueAsDate = new Date();
+  },
+
+  methods: {
+    addTask() {
+      this.$emit("add", this.task);
+      this.task = {
+        title: "",
+        description: "",
+        dueDate: "",
+        createDate: "",
+        note: "",
+      };
+    },
+  },
 };
 </script>
 
