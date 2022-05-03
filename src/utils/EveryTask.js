a5e4ee7e00b62ab56f56f0b351b2b3d061b06715
lisam <lisa.mokshantseva@gmail.com>
2022-05-03 11:24:50 +0200

    Added getTasks

--- src/utils/EveryTask.js
+++ src/utils/EveryTask.js
@@ -1,13 +1,31 @@
 import axios from "axios"
 
-export default class EveryTask {
+let token = null;
 
+export default class EveryTask {
     login(email, password) {
         axios
             .post("http://localhost:8080", {
-                action: 'login',
-                email: email,
+                action: 'login', 
+                email: email, 
                 password: password,
+            })
+            .then((response) => {
+                console.log(response.data);
+                token= response.data.token
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    register(email, password, username) {
+        axios
+            .post("http://localhost:8080/", {
+                action: "register", 
+                email: email, 
+                password: password, 
+                username: username,
             })
             .then((response) => {
                 console.log(response.data);
@@ -17,23 +35,32 @@ export default class EveryTask {
             });
     }
 
-    register(email, password, username) {
+    getTasks(title, description, is_done, due_time, note) {
         axios
-        .post("http://localhost:8080/", {
-          action: "register",
-          email: email,
-          password: password,
-          username: username,
-        })
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+            .post("http://localhost:8080/", {
+                action: "addTask", 
+                token: this.getToken(),
+                title: title, 
+                description: description,
+                is_done: is_done,
+                due_time: due_time,
+                created_time: this.currentTime(),
+                note: note,
+            })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
-    getTasks() {
+    currentTime() {
+      var now = new Date;
+      return now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate() + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    }
 
+    getToken(){
+      return this.token;
     }
 }
